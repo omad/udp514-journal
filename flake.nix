0704dd876d7d1a1103e035948c403f1400aa068d
Damien Ayers <damien@omad.net>
2025-11-16 17:51:45 +1100

    more fix flake

--- flake.nix
+++ flake.nix
@@ -3,29 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    # rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
-      rust-overlay,
+    # rust-overlay,
     }:
     let
-      systems = flake-utils.lib.defaultSystems;
-    in
-    flake-utils.lib.eachSystem systems (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
-        };
-
-        udp514-journal-c = pkgs.stdenv.mkDerivation {
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+      udp514-journal-c =
+        pkgs:
+        pkgs.stdenv.mkDerivation {
           pname = "udp514-journal-c";
           version = "0.2.1"; # From Makefile
           src = self;
@@ -44,95 +40,97 @@
             runHook postInstall
           '';
         };
+    in
+    {
 
-        udp514-journal-go = pkgs.buildGoModule {
-          pname = "udp514-journal-go";
-          version = "0.0.1";
-          src = self;
-          sourceRoot = "${self}/go";
+      packages = forAllSystems (pkgs: {
+        default = udp514-journal-c pkgs;
 
-          vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-        };
+      });
 
-        udp514-journal-rust = pkgs.rustPlatform.buildRustCrate {
-          pname = "udp514-journal-rust";
-          version = "0.1.0";
-          src = self;
-          sourceRoot = "${self}/rust";
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 
-          cargoLock = {
-            lockFile = ./rust/Cargo.lock;
-          };
+        # Provide a NixOS module
+        nixosModules.default =
+          {
+            config,
+            lib,
+            pkgs,
+            ...
+          }:
+          let
+            cfg = config.services.udp514-journal;
+            package = udp514-journal-c pkgs;
+          in
+          {
+            options.services.udp514-journal = {
+              enable = lib.mkEnableOption "udp514-journal service";
 
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [ systemdLibs ];
-        };
-
-        allPackages = {
-          c = udp514-journal-c;
-          go = udp514-journal-go;
-          rust = udp514-journal-rust;
-          default = udp514-journal-c;
-        };
-      in
-      {
-        packages = allPackages;
-
-      }
-    )
-    // {
-      # Provide a NixOS module
-      nixosModules.default =
-        {
-          config,
-          lib,
-          pkgs,
-          ...
-        }:
-        let
-          cfg = config.services.udp514-journal;
-        in
-        {
-          options.services.udp514-journal = {
-            enable = lib.mkEnableOption "udp514-journal service";
-
-            # package = lib.mkOption {
-            #   type = lib.types.package;
-            #   default = self.packagesallPackages.default;
-            #   description = "The udp514-journal package to use.";
-            # };
-          };
-
-          config = lib.mkIf cfg.enable {
-            systemd.services."udp514-journal" = {
-              description = "Forward syslog from network (udp/514) to journal";
-              requires = [
-                "systemd-journald.socket"
-                "udp514-journal.socket"
-              ];
-              after = [ "network.target" ];
-              wantedBy = [ "multi-user.target" ];
-
-              serviceConfig = {
-                Type = "notify";
-                Restart = "always";
-                ExecStart = "${cfg.package}/bin/udp514-journal";
-                DynamicUser = true;
-                ProtectSystem = "full";
-                ProtectHome = true;
-                PrivateDevices = true;
-                NoNewPrivileges = true;
-              };
+              # package = lib.mkOption {
+              #   type = lib.types.package;
+              #   default = self.packagesallPackages.default;
+              #   description = "The udp514-journal package to use.";
+              # };
             };
 
-            systemd.sockets."udp514-journal" = {
-              description = "Forward syslog from network (udp/514) to journal socket";
-              wantedBy = [ "sockets.target" ];
-              socketConfig = {
-                ListenDatagram = "514";
+            config = lib.mkIf cfg.enable {
+              systemd.services."udp514-journal" = {
+                description = "Forward syslog from network (udp/514) to journal";
+                requires = [
+                  "systemd-journald.socket"
+                  "udp514-journal.socket"
+                ];
+                after = [ "network.target" ];
+                wantedBy = [ "multi-user.target" ];
+
+                serviceConfig = {
+                  Type = "notify";
+                  Restart = "always";
+                  ExecStart = "${package}/bin/udp514-journal";
+                  DynamicUser = true;
+                  ProtectSystem = "full";
+                  ProtectHome = true;
+                  PrivateDevices = true;
+                  NoNewPrivileges = true;
+                };
+              };
+
+              systemd.sockets."udp514-journal" = {
+                description = "Forward syslog from network (udp/514) to journal socket";
+                wantedBy = [ "sockets.target" ];
+                socketConfig = {
+                  ListenDatagram = "514";
+                };
               };
             };
           };
-        };
+
+      };
+      overlays.default = final: prev: { };
+
     };
+
+  # udp514-journal-go = pkgs.buildGoModule {
+  #   pname = "udp514-journal-go";
+  #   version = "0.0.1";
+  #   src = self;
+  #   sourceRoot = "${self}/go";
+
+  #   vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  # };
+
+  # udp514-journal-rust = pkgs.rustPlatform.buildRustCrate {
+  #   pname = "udp514-journal-rust";
+  #   version = "0.1.0";
+  #   src = self;
+  #   sourceRoot = "${self}/rust";
+
+  #   cargoLock = {
+  #     lockFile = ./rust/Cargo.lock;
+  #   };
+
+  #   nativeBuildInputs = with pkgs; [ pkg-config ];
+  #   buildInputs = with pkgs; [ systemdLibs ];
+  # };
+
 }
